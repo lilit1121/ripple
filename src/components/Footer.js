24392f59d4626f47f206ce834dbdcbde11c82606
lilit1121 <tamrazyan.lilit.95@gmail.com>
2024-05-07 11:53:54 +0400

    optimize

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,29 +1,43 @@
+import React from "react";
 import { Link } from "react-router-dom";
-import footer from "../assets/styles/components/Footer.module.css";
-
+import footerStyles from "../assets/styles/components/Footer.module.css";
 import Discord from "../assets/images/socialmedia/Discord.svg";
 import Twitter from "../assets/images/socialmedia/Twitter.svg";
-import In from "../assets/images/socialmedia/ln.svg";
-
+import LinkedIn from "../assets/images/socialmedia/ln.svg";
 import Message from "./svg/Message";
-
 import Select from "../components/base/Select";
+
+function SocialMediaLink({ to, image, alt }) {
+  return (
+    <Link to={to} className={footerStyles.socialMediaLink}>
+      <img src={image} alt={alt} />
+    </Link>
+  );
+}
+
+function FooterLink({ to, children }) {
+  return (
+    <li>
+      <Link to={to}>{children}</Link>
+    </li>
+  );
+}
 
 function Footer() {
   const currentYear = new Date().getFullYear();
-  const list = [
-    {
-      name: "Currency - USD",
-      value: "test",
-    },
+  const socialMediaLinks = [
+    { to: "/", image: Discord, alt: "discord" },
+    { to: "/", image: Twitter, alt: "twitter" },
+    { to: "/", image: LinkedIn, alt: "linkedin" },
   ];
+
   return (
-    <footer className={footer.footer}>
-      <div className={`flex flex-wrap ${footer.footer_container}`}>
+    <footer className={footerStyles.footer}>
+      <div className={`flex flex-wrap ${footerStyles.footer_container}`}>
         <div className="flex">
           <div>
-            <h6 className={footer.footer_title}>Subscribe to updates</h6>
-            <div className={`flex ${footer.footer_subscribe}`}>
+            <h6 className={footerStyles.footer_title}>Subscribe to updates</h6>
+            <div className={`flex ${footerStyles.footer_subscribe}`}>
               <div className="relative">
                 <input type="text" placeholder="Enter your e-mail" />
                 <Message color="#5D5D5B" />
@@ -32,74 +46,44 @@ function Footer() {
             </div>
           </div>
           <div>
-            <h6 className={footer.footer_title}>Follow us</h6>
-            <div className={footer.footer_socialMedia}>
-              <Link to="/">
-                <img src={Discord} alt="discord" />
-              </Link>
-              <Link to="/">
-                <img src={Twitter} alt="twitter" />
-              </Link>
-              <Link to="/">
-                <img src={In} alt="in" />
-              </Link>
+            <h6 className={footerStyles.footer_title}>Follow us</h6>
+            <div className={footerStyles.footer_socialMedia}>
+              {socialMediaLinks.map((link, index) => (
+                <SocialMediaLink key={index} {...link} />
+              ))}
             </div>
           </div>
         </div>
         <div className="flex flex-wrap">
           <div>
-            <h6 className={footer.footer_title}>Marketplace</h6>
+            <h6 className={footerStyles.footer_title}>Marketplace</h6>
             <ul className="list-unstyled">
-              <li>
-                <Link to="/">Explore</Link>
-              </li>
-              <li>
-                <Link to="/">Help Center</Link>
-              </li>
-              <li>
-                <Link to="/">Become a Partner</Link>
-              </li>
-              <li>
-                <Link to="/">About Us</Link>
-              </li>
-              <li>
-                <Link to="/">Platform Status</Link>
-              </li>
+              <FooterLink to="/">Explore</FooterLink>
+              <FooterLink to="/">Help Center</FooterLink>
+              <FooterLink to="/">Become a Partner</FooterLink>
+              <FooterLink to="/">About Us</FooterLink>
+              <FooterLink to="/">Platform Status</FooterLink>
             </ul>
           </div>
-
           <div>
-            <h6 className={footer.footer_title}>Community</h6>
+            <h6 className={footerStyles.footer_title}>Community</h6>
             <ul className="list-unstyled">
-              <li>
-                <Link to="/">Profile</Link>
-              </li>
-              <li>
-                <Link to="/">Favorites</Link>
-              </li>
-              <li>
-                <Link to="/">Watchlist</Link>
-              </li>
-              <li>
-                <Link to="/">My Collections</Link>
-              </li>
-              <li>
-                <Link to="/">Rankings</Link>
-              </li>
-              <li>
-                <Link to="/">Activity</Link>
-              </li>
+              <FooterLink to="/">Profile</FooterLink>
+              <FooterLink to="/">Favorites</FooterLink>
+              <FooterLink to="/">Watchlist</FooterLink>
+              <FooterLink to="/">My Collections</FooterLink>
+              <FooterLink to="/">Rankings</FooterLink>
+              <FooterLink to="/">Activity</FooterLink>
             </ul>
           </div>
-
-          <div className={footer.footer__reimagia_block}>
-            <h6 className={footer.footer_title}>REIMAGIA</h6>
+          <div className={footerStyles.footer__reimagia_block}>
+            <h6 className={footerStyles.footer_title}>REIMAGIA</h6>
             <p>
               The world's first and largest ethical Generative AI marketplace
               for creators worldwide where anything is possible and all are
               welcome
             </p>
-            <Select defaultValue="Currency - USD" list={list} />
+            <Select defaultValue="Currency - USD" list={[{ name: "Currency - USD", value: "test" }]} />
           </div>
         </div>
       </div>
--- src/views/Login.js
+++ src/views/Login.js
@@ -1,17 +1,67 @@
+import React from "react";
 import { Formik, Field, ErrorMessage } from "formik";
-
 import { Link } from "react-router-dom";
-
 import { socialMediaLogin } from "../constants/login";
-
-import "../assets/styles/views/Auth.css";
-import login from "../assets/styles/views/Login.module.css";
-
+import { loginSchema } from "../utils/ValidationScheme";
 import AuthHeader from "../components/auth/AuthHeader";
 import AuthInput from "../components/auth/AuthInput";
+import loginStyles from "../assets/styles/views/Login.module.css";
 
-import { loginSchema } from "../utils/ValidationScheme";
+function LoginForm({ handleSubmit, values, setFieldValue }) {
+  return (
+    <form autoComplete="off" onSubmit={handleSubmit}>
+      <div className={loginStyles.input_container}>
+        <div>
+          <AuthInput
+            id="email"
+            name="email"
+            label="Email"
+            placeholder="Enter your email"
+            type="text"
+            value={values.email}
+            handleChange={(e) => setFieldValue("email", e)}
+            as={Field}
+          />
+          <ErrorMessage
+            name="email"
+            component="div"
+            className="error-message"
+          />
+        </div>
+        <div>
+          <AuthInput
+            id="password"
+            name="password"
+            label="Password"
+            placeholder="Enter your password"
+            type="password"
+            value={values.password}
+            handleChange={(e) => setFieldValue("password", e)}
+            as={Field}
+          />
+          <ErrorMessage
+            name="password"
+            component="div"
+            className="error-message"
+          />
+        </div>
+      </div>
 
+      <div className={loginStyles.login_block}>
+        <div className={loginStyles.login_block__remember}>
+          <Field id="remember" name="remember" type="checkbox" />
+          <label htmlFor="remember">Remember me</label>
+        </div>
+        <Link className={loginStyles.login_block__recover} to="/">
+          Recover password
+        </Link>
+      </div>
+      <button className="blue-btn btn" type="submit">
+        Sign In
+      </button>
+    </form>
+  );
+}
 
 function Login() {
   return (
@@ -32,75 +82,33 @@ function Login() {
         }}
         validationSchema={loginSchema}
       >
-        {({ values, handleSubmit, setFieldValue }) => (
-          <form autoComplete="off" onSubmit={handleSubmit}>
-            <div className={login.input_container}>
-              <div>
-                <AuthInput
-                  id="email"
-                  name="email"
-                  label="Email"
-                  placeholder="Enter your email"
-                  type="text"
-                  value={values.email}
-                  handleChange={(e) => setFieldValue("email", e)}
-                  as={Field}
-                />
-                <ErrorMessage
-                  name="email"
-                  component="div"
-                  className="error-message"
-                />
-              </div>
-              <div>
-                <AuthInput
-                  id="password"
-                  name="password"
-                  label="Password"
-                  placeholder="Enter your password"
-                  type="password"
-                  value={values.password}
-                  handleChange={(e) => setFieldValue("password", e)}
-                  as={Field}
-                />
-                <ErrorMessage
-                  name="password"
-                  component="div"
-                  className="error-message"
-                />
-              </div>
-            </div>
-
-            <div className={login.login_block}>
-              <div className={login.login_block__remember}>
-                <Field id="remember" name="remember" type="checkbox" />
-                <label htmlFor="remember">Remember me</label>
-              </div>
-              <Link className={login.login_block__recover} to="/">
-                Recover password
-              </Link>
-            </div>
-            <button className="blue-btn btn" type="submit">
-              Sign In
-            </button>
-          </form>
+        {(formikProps) => (
+          <LoginForm
+            handleSubmit={formikProps.handleSubmit}
+            values={formikProps.values}
+            setFieldValue={formikProps.setFieldValue}
+          />
         )}
       </Formik>
-      <div className={login.login_block__or}>
-        <div className={login.line} />
+      <div className={loginStyles.login_block__or}>
+        <div className={loginStyles.line} />
         or
-        <div className={login.line} />
+        <div className={loginStyles.line} />
       </div>
-      <div className={login.social_media_container}>
+      <div className={loginStyles.social_media_container}>
         {socialMediaLogin.map((data, index) => (
-          <a className={login.social_media_btn} key={index} href={data.url}>
+          <a
+            className={loginStyles.social_media_btn}
+            key={index}
+            href={data.url}
+          >
             <img src={data.icon} alt={data.alt} />
             {data.title}
           </a>
         ))}
       </div>
 
-      <p className={login.signup}>
+      <p className={loginStyles.signup}>
         You don’t have an account? <Link to="/registration">Sign Up</Link>
       </p>
     </div>
